1e5c66ad89c4bcace156a2c29c8e7a878f19a262
ShopAccVanQuoc <l.ethimonleo01@gmail.com>
2024-05-30 00:18:30 +0700

    Create playerduo.js

---
+++ playerduo.js
@@ -0,0 +1,4 @@
+var obj = JSON.parse($response.body);
+
+obj= {"success":true,"result":{"_id":"64a6e41d8e1f4853840c8e7e","playerSetting":{"url":"64a6e41d8e1f4853840c8e7e"},"trustTypes":[],"isOpenReceivingMessage":true,"rank":"0.png","avatar":"avatar26.png","name":"Levanquochihi","city":"5b99f9e35180d13ea22a9814","country":"5b99f97c978dff3d115261c7","url":"xquocdeeptrai","totalMoney":4250,"email":"levanquocht21@gmail.com","role":"normal","user":"ee8d9528-a89b-43b8-9b07-4c55730d58fb","username":"levanquochihi","subscribingThemes":[],"themes":[],"pageUserId":"deprecated__","verifications":[{"type":"mail","verifiedAt":"2024-05-29T17:15:25.382Z"}],"timedate":"2024-05-29T17:15:25.383Z","timestamp":1717002925383}}
+$done({body: JSON.stringify(obj)});
