743eb0adfeb86b45fa45600aab783bd933f18d31
ShopAccVanQuoc <l.ethimonleo01@gmail.com>
2024-05-30 14:11:36 +0700

    Create gptlimit.js

---
+++ gpt/gptlimit.js
@@ -0,0 +1,8 @@
+{
+    "message_cap": 99999.0,
+    "message_cap_window": 99999.0,
+    "message_disclaimer": {
+        "textarea": "Crack By ShopAccVannQuoc",
+        "model-switcher": "Bạn đã đạt đến giới hạn GPT-4, ngưỡng cho phép tất cả người dùng ChatGPT Plus cơ hội dùng thử mô hình.\n\nVui lòng kiểm tra lại sớm."
+    }
+}
